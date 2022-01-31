3acc7fd8f5643daa9c48c45ba6815f742b792b95
Nitya <gorenityanand96@gmail.com>
2022-01-31 14:30:50 +0530

    [Nityanand]:PerfectNumber Added

--- Program.cs
+++ Program.cs
@@ -6,8 +6,31 @@ namespace LogicalPrograms
     {
         public static void Main(string[] args)
         {
-            Fibonacci f1 = new Fibonacci();
-            f1.fibo();
+            Console.WriteLine("Enter number for select Option: \n 1. Fibonacci \n 2. Perfect Number \n 3. Exit.\n ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            switch (num)
+            {
+                case 1:
+                    Fibonacci f1 = new Fibonacci();
+                    f1.fibo();
+                    break;
+                case 2:
+                    PerfectNumber p1= new PerfectNumber();
+                    p1.perfect();
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.Console.WriteLine("Choose correct option..");
+                    break;
+            }
+            
+            
+            
+
+            
+            
         }
     }
 }
